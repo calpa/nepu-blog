918441e8477ce1909db79bb9b7681172def8b93e
Calpa Liu <calpaliu@gmail.com>
2017-09-17 17:04:49 +0800

    Initialization

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ss from './index.sass'
+
+console.log(ss.example);
+
+export const IndexPage = () => (
+  <div className={ss.example}>
+    Calpa's Blog
+  </div>
+)
+
+export default IndexPage
