af143243bc6eb4b55c8e5890917f511545bc5e07
Calpa Liu <calpaliu@gmail.com>
2017-09-19 20:58:37 +0800

    Fix sass not find problem

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Layout from '../layouts';
 
+import ss from './home.sass';
+
 const Home = () => (
   <Layout>
     <div>Home</div>
+    <div className={ss.example}>Example</div>
   </Layout>
 );
 
